59d41aafee0570c7e47c1a183b4f7fd480183797
권찬호 <rnjscksdud17@gmail.com>
2024-10-02 16:32:46 +0900

    💡 model 추가

--- src/main/kotlin/kr/ac/kumoh/s20210084/web24w05thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210084/web24w05thymeleaf/controller/SongController.kt
@@ -1,12 +1,14 @@
 package kr.ac.kumoh.s20210084.web24w05thymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/song/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model: Model): String {
+        model.addAttribute("songTitle","odoriko")
         return "random"
     }
 }
